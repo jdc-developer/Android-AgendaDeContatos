fa7b177965d081bec9ea7aa1fb03a57d3a91f7de
Jorge Do Carmo <tidesconfie@gmail.com>
2019-02-20 18:17:54 -0300

    Camera e permissões

--- app/src/main/java/jdc/agendadecontatos/EditActivity.java
+++ app/src/main/java/jdc/agendadecontatos/EditActivity.java
@@ -1,6 +1,14 @@
 package jdc.agendadecontatos;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +21,8 @@ public class EditActivity extends AppCompatActivity {
 
     private ContatoInfo contato;
 
+    private View layout;
+
     private ImageButton foto;
     private EditText nome;
     private EditText ref;
@@ -22,12 +32,17 @@ public class EditActivity extends AppCompatActivity {
 
     private Button salvar;
 
+    private final int CAMERA = 1;
+    private final int GALERIA = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
         contato = getIntent().getParcelableExtra("contato");
+
+        layout = findViewById(R.id.layout);
 
         foto = findViewById(R.id.ftContato);
         nome = findViewById(R.id.nomeContato);
@@ -41,6 +56,13 @@ public class EditActivity extends AppCompatActivity {
         email.setText(contato.getEmail());
         fone.setText(contato.getTelefone());
         endereco.setText(contato.getEndereco());
+
+        foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertaImagem();
+            }
+        });
 
         salvar = findViewById(R.id.btnSalvar);
         salvar.setOnClickListener(new View.OnClickListener() {
@@ -63,5 +85,71 @@ public class EditActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void alertaImagem() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditActivity.this);
+        builder.setTitle("Selecione a fonte da imagem");
+
+        builder.setPositiveButton("Camera", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tirarFoto();
+            }
+        });
+        builder.setNegativeButton("Galeria", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                galeria();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void tirarFoto() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            requestCameraPermission();
+        } else {
+            showCamera();
+        }
+    }
+
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            Snackbar.make(layout, "É necessário permitir para utilizar a câmera!",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ActivityCompat.requestPermissions(EditActivity.this,
+                            new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            CAMERA);
+                }
+            }).show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    CAMERA);
+        }
+    }
+
+    private void showCamera() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, CAMERA);
+    }
+
+    private void galeria() {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    tirarFoto();
+                }
+        }
     }
 }
