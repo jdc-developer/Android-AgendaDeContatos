4f3570d7e81305a41851eeee88f107b3950af1e7
Jorge Do Carmo <tidesconfie@gmail.com>
2019-02-20 16:45:13 -0300

    Tela de edição de Contato

--- app/src/main/java/jdc/agendadecontatos/ContatoInfo.java
+++ app/src/main/java/jdc/agendadecontatos/ContatoInfo.java
@@ -1,4 +1,59 @@
 package jdc.agendadecontatos;
 
 public class ContatoInfo {
+
+    private String nome;
+    private String ref;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private String foto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
